f4b85d042ebcab17511ceb0075b5c4a254dc8247
limq <limq@cgnfs.com.cn>
2018-09-29 16:33:14 +0800

    fwebpack

---
+++ firstWebpackVue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    App.vue
+    怎么使用组件呢
+    <div id="components-demo">
+      <button-counter></button-counter>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ firstWebpackVue/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+Vue.component('button-counter',{
+	data:function (){
+		return {count:0}
+	},
+	template:'<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
