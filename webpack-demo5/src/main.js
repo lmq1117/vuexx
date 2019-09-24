de0b00baea38e48dfcfe446fd5c44f57b55bc220
limq <limq@cgnfs.com.cn>
2019-09-24 10:24:00 +0800

    打包文件 并使用HtmlWebpackPlugin插件

---
+++ webpack-demo5/src/main.js
@@ -0,0 +1,3 @@
+var bar = require('./bar.js')
+import './css/style.css'
+bar()
---
+++ webpack-demo5/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry:'./src/main.js',
+    mode:'development',
+    output:{
+        path:path.join(__dirname,'./dist/'),
+        filename:'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    //先认识再装载
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use:[
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({template: './index.html'})
+    ]
+}
