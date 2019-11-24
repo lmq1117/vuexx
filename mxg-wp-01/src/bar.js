3afd7301c9d3367637b7f12cc54b8c7d207bcccd
mingquan.li <mingquan.li@5262.com>
2019-11-24 22:35:46 +0800

    2.2.3 引入打包配置文件 webpack.config.js

--- mxg-wp-01/src/bar.js
+++ mxg-wp-01/src/bar.js
@@ -19,6 +19,43 @@ n' to enable defaults for each environment.
 You can also set it to 'none' to disable any default behavior. Learn more: https://webpack.js.org/configuration/mode/
 */
 
+//打包命令
+//D:\vue\vuexx\mxg-wp-01>node_modules\.bin\webpack
+/*
+D:\vue\vuexx\mxg-wp-01>node_modules\.bin\webpack
+Hash: 0ca3eced5f5e3ff75721
+Version: webpack 4.35.2
+Time: 419ms
+Built at: 2019-11-24 22:29:24
+    Asset        Size  Chunks             Chunk Names
+bundle.js  1010 bytes       0  [emitted]  main
+Entrypoint main = bundle.js
+[0] ./src/main.js 33 bytes {0} [built]
+[1] ./src/bar.js 910 bytes {0} [built]
+
+WARNING in configuration
+The 'mode' option has not been set, webpack will fallback to 'production' for this value. Set 'mode' option to 'development' or 'productio
+n' to enable defaults for each environment.
+You can also set it to 'none' to disable any default behavior. Learn more: https://webpack.js.org/configuration/mode/
+
+D:\vue\vuexx\mxg-wp-01>node_modules\.bin\webpack
+Hash: 8520fa0da4288a95631f
+Version: webpack 4.35.2
+Time: 400ms
+Built at: 2019-11-24 22:29:41
+    Asset        Size  Chunks             Chunk Names
+bundle.js  1010 bytes       0  [emitted]  main
+Entrypoint main = bundle.js
+[0] ./src/main.js 33 bytes {0} [built]
+[1] ./src/bar.js 910 bytes {0} [built]
+
+WARNING in configuration
+The 'mode' option has not been set, webpack will fallback to 'production' for this value. Set 'mode' option to 'development' or 'productio
+n' to enable defaults for each environment.
+You can also set it to 'none' to disable any default behavior. Learn more: https://webpack.js.org/configuration/mode/
+
+*/
+
 module.exports = function () {
-    console.log('我是 bar 模块02')
+    console.log('我是 bar 模块03')
 }
