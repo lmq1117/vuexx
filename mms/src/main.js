b088b27c8e9d160cbb6856e6141bc4382323b486
mingquan.li <mingquan.li@5262.com>
2019-11-28 23:21:04 +0800

    引入element

--- mms/src/main.js
+++ mms/src/main.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
+
+import ElementUI from "element-ui";//组件库
+import 'element-ui/lib/theme-chalk/index.css';//样式
+
 import App from "./App.vue";
 import router from "./router";
 
-Vue.config.productionTip = false;
+Vue.use(ElementUI);
+
+//消息提示与环境配置false 开发，true 生产
+Vue.config.productionTip = process.env.NODE_ENV === 'production';
+
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount("#app");
