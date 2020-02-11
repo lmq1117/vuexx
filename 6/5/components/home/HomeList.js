1a4283b62058d9b0b0e87a3db1861b5677b54f6d
mingquan.li <mingquan.li@5262.com>
2020-02-11 16:28:15 +0800

    props传递函数实现删除功能 数据在哪儿定义就在哪儿删、多层级组件需要层层传递

--- 6/5/components/home/HomeList.js
+++ 6/5/components/home/HomeList.js
@@ -7,21 +7,37 @@
               <th>#</th>
               <th>姓名</th>
               <th>工资</th>
+              <th>操作</th>
             </tr>
           </thead>
           <tbody>
-            <list-item v-for="(emp,index) in empList" :key="emp.id" :emp="emp"></list-item>
+            <list-item v-for="(emp,index) in empList" 
+            :key="emp.id" 
+            :emp="emp" 
+            :deleteEmp="deleteEmp"
+            :index="index"
+            ></list-item>
           </tbody>
         </table>
       </div>`,
-      data (){
-        return {
-          name :'小Q'
-        }
+    data() {
+      return {
+        name: '小Q'
+      }
+    },
+    components: {
+      ListItem
+    },
+    // props:['empList','deleteEmp']
+    props: {
+      empList: {
+        type: Array,
+        require: true
       },
-      components:{
-        ListItem
-      },
-      props:['empList']
+      deleteEmp: {
+        type: Function,
+        require: true
+      }
+    }
   }
 })()
--- 6/5/components/home/ListItem.js
+++ 6/5/components/home/ListItem.js
@@ -3,9 +3,33 @@
     <td>{{emp.id}}</td>
     <td>{{emp.name}}</td>
     <td>{{emp.salary}}</td>
+    <td>
+    <a href="#" @click="deleteItem">删除</a>
+    </td>
   </tr>`
   window.ListItem = {
       template,
-      props:['emp']
+    //   props:['emp',{name:'deleteEmp',type:Function},'index'],
+    props:{
+        emp:{
+            type:Object,
+            require:true
+        },
+        deleteEmp:{
+            type:Function,
+            require:true
+        },
+        index:{
+            type:Number,
+            require:true
+        }
+    },
+      methods: {
+        deleteItem(){
+            if(window.confirm(`确定删除姓名为: ${this.emp.name} 的记录吗?`)){
+                this.deleteEmp(this.index)
+            }
+        }
+      },
   }
 })()
