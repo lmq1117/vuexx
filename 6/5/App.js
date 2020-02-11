b1dab2d6cb81a5a16cdb24159bc8efdcf8990e1a
mingquan.li <mingquan.li@5262.com>
2020-02-11 20:33:00 +0800

    动态插槽title

--- 6/5/App.js
+++ 6/5/App.js
@@ -9,7 +9,7 @@
             <app-left></app-left>
             <!-- 右侧主页区域 分上下两个区域 -->
             <app-home>
-            <h1 slot="dash-board-title" class="page-header">Dashboard Title </h1>
+            <h1 slot="dash-board-title" class="page-header">{{title}}</h1>
             </app-home>
           </div>
         </div></div>`,
@@ -17,6 +17,11 @@
             AppNavbar,
             AppLeft,
             AppHome
+          },
+          data(){
+            return {
+              title:"仪表盘"
+            }
           }
     }
 })()
